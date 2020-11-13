1cf6c36b147917eceddf36a79cfb10c4ea73577f
konst <konstantin.mikheev@icord.cz>
2020-11-13 09:17:56 +0100

    push new improvements

--- server/src/utils/api.ts
+++ server/src/utils/api.ts
@@ -1,10 +1,14 @@
 import { IDictionary } from 'src/custom-types/dictionary';
 
-export function buildQueryString(items: IDictionary<string>[]) {
+export function buildQueryString(items: IDictionary<string | object>[]) {
   const joined = items
-    .map(it => {
+    .map((it) => {
       const key = Object.keys(it)[0];
-      return `${key}=${encodeURI(it[key])}`;
+      return `${key}=${
+        typeof it[key] === 'object'
+          ? JSON.stringify(it[key])
+          : encodeURI(it[key] as string)
+      }`;
     })
     .join('&');
   return joined.length > 0 ? '?' + joined : '';
